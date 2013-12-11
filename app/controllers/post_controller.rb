78db578564fcbff614200de1307a820164cc2614
Erik Hunter <erik.hunter@colorado.edu>
2013-12-11 11:31:31 -0700

    got the create post view working

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,16 @@
 class PostController < ApplicationController
   def create
+  	params[:post][:user_id] = current_user.id
+  	if @post = Post.create(params[:post])
+  		@post.user_id = current_user.id
+  		redirect_to '/'
+  	else
+  		redirect_to new_post_path
+  	end
+  end
+  
+  def new
+  	@post = Post.new
   end
   
   def index
